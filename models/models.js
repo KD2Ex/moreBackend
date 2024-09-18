d19cbd1c4932fad5aa6952796da3250879a8c597
kd2ex <kountdown@mail.ru>
2024-09-18 12:41:05 +0300

    length of the desc increased

--- models/models.js
+++ models/models.js
@@ -82,8 +82,8 @@ const LocaleTextTechnique = sequelize.define('localeTextTechnique', {
 
 const LocaleTextPainting = sequelize.define('localeTextPainting', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    title: {type: DataTypes.STRING, allowNull: false},
-    desc: {type: DataTypes.STRING, allowNull: false},
+    title: {type: DataTypes.STRING(100), allowNull: false},
+    desc: {type: DataTypes.STRING(2000), allowNull: false},
     price: {type: DataTypes.STRING, allowNull: false},
 })
 
